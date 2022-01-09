3364420de791e9d9da266fa7d7f2359f9aa439f1
erick_braga <@Erickmatheus07>
2022-01-09 13:43:29 -0300

    Final aula 1 atividade 3

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './assets/css/base/base.css'
+import Home from './paginas/Home'
+import Sobre from './paginas/Sobre'
 
 function App() {
+  const Router = () => {
+    const location = window.location.pathname;
+    if (location === '/sobre') {
+      return <Sobre />;
+    } else {
+      return <Home />;
+    }
+  }
   return (
   <>
-
+    { Router() }
   </>
   )
 }
