227adc6991bd38a99c87c9c07baf7964bde77ff6
erick_braga <@Erickmatheus07>
2022-01-10 07:44:32 -0300

    Finalizado aula 4 atividade 2

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -6,6 +6,7 @@ import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
 import Pagina404 from './paginas/Pagina404'
 import Cabecalho from './components/Cabecalho'
+import Post from './paginas/Post'
 
 function App() {
 
@@ -18,6 +19,9 @@ function App() {
         </Route>
         <Route path="/sobre">
           <Sobre />
+        </Route>
+        <Route path="/posts/:id">
+          <Post />
         </Route>
         <Route>
           <Pagina404 />
---
+++ petshop/src/paginas/Post.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import { busca } from '../api/api';
+import '../assets/css/post.css';
+import { useParams } from 'react-router-dom';
+
+const Post = () => {
+  const { id } = useParams();
+  const[post, setPost] = useState({})
+
+  useEffect(() => {
+    busca(`/posts/${id}`, setPost);
+  }, [id])
+
+  return (
+    <main className="container flex flex--centro">
+      <article className="cartao post">
+        <h2 className="cartao__titulo">{post.title}</h2>
+        <p className="cartao__texto">{post.body}</p>
+      </article>
+    </main>
+  )
+}
+
+export default Post;
