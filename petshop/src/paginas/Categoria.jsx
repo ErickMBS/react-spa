e25fc2a5d68ef2ca637e9b442fc60d9e2a84e3ae
erick_braga <@Erickmatheus07>
2022-01-10 08:24:40 -0300

    Curso finalizado

--- petshop/src/paginas/Categoria.jsx
+++ petshop/src/paginas/Categoria.jsx
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
-import '../assets/css/blog.css';
-import ListaCategorias from '../components/ListaCategorias';
-import { useParams, Route, useRouteMatch } from 'react-router-dom';
-import ListaPost from '../components/ListaPost';
-import { busca } from '../api/api';
+import React, { useEffect, useState } from "react";
+import "../assets/css/blog.css";
+import ListaCategorias from "../components/ListaCategorias";
+import { useParams, Route, useRouteMatch, Link, Switch } from "react-router-dom";
+import ListaPost from "../components/ListaPost";
+import { busca } from "../api/api";
+import SubCategoria from "./SubCategoria";
 
 function Categoria() {
   const { id } = useParams();
-  const { path } = useRouteMatch();
-  const [ subcategorias, setSubCategorias ] = useState([]);
+  const { url, path } = useRouteMatch();
+  const [subcategorias, setSubCategorias] = useState([]);
 
   useEffect(() => {
     busca(`/categorias/${id}`, (categoria) => {
       setSubCategorias(categoria.subcategorias);
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <>
@@ -23,9 +24,26 @@ function Categoria() {
       </div>
 
       <ListaCategorias />
-      <Route exact path={`${path}`}>
-        <ListaPost url={`posts?categoria=${id}`} />
-      </Route>
+      <ul className="lista-categorias container flex">
+        {subcategorias.map((subcategoria) => (
+          <li
+            className={`lista-categorias__categoria lista-categorias__categoria--${id}`}
+            key={subcategoria}
+          >
+            <Link to={`${url}/${subcategoria}`}>{subcategoria}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <Switch>
+        <Route exact path={`${path}`}>
+          <ListaPost url={`posts?categoria=${id}`} />
+        </Route>
+
+        <Route exact path={`${path}/:subcategoria`}>
+          <SubCategoria />
+        </Route>
+      </Switch>
     </>
   );
 }
