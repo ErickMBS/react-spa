0d6640192f9c63f63fedb137353ec4004356920d
erick_braga <@Erickmatheus07>
2022-01-10 07:48:38 -0300

    Finalizado aula 4 atividade 3

--- petshop/src/paginas/Post.jsx
+++ petshop/src/paginas/Post.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { busca } from '../api/api';
 import '../assets/css/post.css';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 const Post = () => {
+  let history = useHistory();
   const { id } = useParams();
   const[post, setPost] = useState({})
 
   useEffect(() => {
-    busca(`/posts/${id}`, setPost);
+    busca(`/posts/${id}`, setPost)
+      .catch(() => {
+        history.push('/404')
+      });
   }, [id])
 
   return (
